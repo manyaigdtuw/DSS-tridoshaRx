cc2738b68a6d2d9c5a06233f1930f9adef0bd949
Manya Singh <142112751+manyaigdtuw@users.noreply.github.com>
2025-08-06 12:06:41 +0530

    updated pool for render

--- backend/server.js
+++ backend/server.js
@@ -899,7 +899,8 @@ app.post('/api/add-entry', async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
