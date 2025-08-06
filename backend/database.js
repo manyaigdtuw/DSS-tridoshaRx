dcdeb6585e323b0277f49ee49f0252907fcad07b
Manya Singh <142112751+manyaigdtuw@users.noreply.github.com>
2025-08-06 11:48:26 +0530

    updated pool for render

--- backend/database.js
+++ backend/database.js
@@ -2,11 +2,11 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'namasteportal',
-  password: process.env.DB_PASSWORD || 'admin123',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL, // Render sets this automatically
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 export default pool;
