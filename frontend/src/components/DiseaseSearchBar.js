12ac4aa31f5d0f33a7a3e333fb4868f99766c999
Manya Singh <142112751+manyaigdtuw@users.noreply.github.com>
2025-08-06 12:30:42 +0530

    fixed disease search

--- frontend/src/components/DiseaseSearchBar.js
+++ frontend/src/components/DiseaseSearchBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./SearchBar.css";
 
-const API_BASE_URL = "https://dss-tridosharx.onrender.com/";
+const API_BASE_URL = "http://localhost:5000";
 
 const DiseaseSearchBar = ({ onSearch }) => {
   const [inputValue, setInputValue] = useState("");
